423f9442b56c018cbc4c51936467b76d94bc9b2f
Jesse-Maurice Iyoha <iyohajessy@gmail.com>
2023-02-02 19:43:34 +0100

    last commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mobileMenu = document.querySelector(".ham");
 const closeMenu = document.querySelector(".close-menu");
 const menu = document.querySelector(".nav-mobile");
 const overlay = document.querySelector(".overlay");
+const hero = document.querySelector(".hero-img");
 
 mobileMenu.addEventListener("click", () => {
   menu.classList.add("hidden");
@@ -9,6 +10,11 @@ mobileMenu.addEventListener("click", () => {
 });
 
 closeMenu.addEventListener("click", () => {
+  menu.classList.remove("hidden");
+  overlay.style.display = "none";
+});
+
+hero.addEventListener("click", () => {
   menu.classList.remove("hidden");
   overlay.style.display = "none";
 });
